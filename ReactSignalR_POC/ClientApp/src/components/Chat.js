141fc08d5a573d9d6c56d88054325056f2a8448e
Kieran <kierantsai@gmail.com>
2022-01-10 20:13:44 +1300

    Add project files.

---
+++ ReactSignalR_POC/ClientApp/src/components/Chat.js
@@ -0,0 +1,93 @@
+﻿import React, { useState, useEffect } from 'react';
+import { HubConnectionBuilder } from '@microsoft/signalr';
+
+import styles from './Chat.module.scss';
+
+function Chat(props) {
+
+    const [connection, setConnection] = useState(null);
+    const [user, setUser] = useState("");
+    const [message, setMessage] = useState("");
+    const [connected, setConnected] = useState(false);
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState(null);
+    const [messagesReceived, setMessagesReceived] = useState(0);
+
+    useEffect(() => {
+        const newConnection = new HubConnectionBuilder()
+            .withUrl("/chatHub")
+            .withAutomaticReconnect()
+            .build();
+        setConnection(newConnection);
+    }, []);
+
+    useEffect(() => {
+        if (connection) {
+            connection.start().then(() => {
+                setConnected(true);
+                console.log('connection started!');
+
+                connection.on("ReceiveMessage", (user, message) => {
+                    console.log('message received.');
+
+                    setNewMessage({ user, message });
+
+                });
+
+            }).catch(err => {
+                console.error(err);
+            });
+        }
+    }, [connection]);
+
+    useEffect(() => {
+        if (newMessage) {
+
+            console.log(newMessage.message);
+            setMessages([...messages, newMessage]);
+            setMessagesReceived(messagesReceived + 1);
+
+        }
+    }, [newMessage]);
+
+    function SendMessage(event) {
+
+        event.preventDefault();
+
+        if (!user || !message)
+            return;
+
+        connection.invoke("SendMessage", user, message).catch(err => {
+            console.log(err);
+        });
+
+        console.log('message sent!');
+    }
+
+    return (
+        <div className={styles.Chat}>
+            <h1>This is the chat page</h1>
+            <div className="Input">
+                <label>User</label>
+                <input type="text" value={user} onChange={e => setUser(e.target.value)} />
+                <label>Message:</label>
+                <input type="text" value={message} onChange={e => setMessage(e.target.value)} />
+                <button type="button" onClick={SendMessage} disabled={!connected}>Send</button>  
+            </div>
+            <div className="MessageList">
+                <ul>
+                {
+                    messages.map((msg, index) => 
+                        <li key={`m${index}`}>{`${msg.user}: ${msg.message}`}</li>
+                    )
+                }
+                </ul>
+            </div>
+            <div>
+                Messages Received: { messagesReceived }
+            </div>
+        </div>
+        );
+}
+
+export { Chat };
---
+++ ReactSignalR_POC/Worker.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Hosting;
+using ReactSignalR_POC.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReactSignalR_POC
+{
+    public class Worker : BackgroundService
+    {
+        private IHubContext<ChatHub> _chatHub;
+
+        public Worker(IHubContext<ChatHub> chatHub)
+        {
+            _chatHub = chatHub;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await _chatHub.Clients.All.SendAsync("ReceiveMessage", "System", DateTime.Now.ToString("O"));
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
